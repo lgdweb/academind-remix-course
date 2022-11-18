fc6d43f431957d78429e08b783b445f7ed76f7f7
Titux Metal (MBP) <github@lgdweb.fr>
2022-11-18 06:57:48 +0100

    Add the main navigation component with active link style

---
+++ src/components/MainNavigation.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from '@remix-run/react'
+import tw from 'tailwind-styled-components'
+
+const Container = tw.nav`py-8 px-4 flex justify-center`
+const Items = tw.ul`list-none m-0 p-0 flex gap-6 justify-center`
+const activeLinkClass = ({ isActive }) =>
+  `text-2xl font-bold no-underline hover:text-orange-500 ${
+    isActive ? 'text-orange-500' : 'text-orange-300'
+  }`
+
+const MainNavigation = () => (
+  <Container>
+    <Items>
+      <li>
+        <NavLink className={activeLinkClass} to='/'>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink className={activeLinkClass} to='/notes'>
+          My Notes
+        </NavLink>
+      </li>
+    </Items>
+  </Container>
+)
+
+export default MainNavigation
