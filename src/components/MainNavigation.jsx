2bf99467bd36476fef5d29830ee079dfe6bf2adb
Titux Metal (MBP) <github@lgdweb.fr>
2022-11-18 12:03:51 +0100

    Fix link color in main navigation, change for a tiny ligther color for accessibility purposes

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -4,8 +4,8 @@ import tw from 'tailwind-styled-components'
 const Container = tw.nav`py-8 px-4 flex justify-center`
 const Items = tw.ul`list-none m-0 p-0 flex gap-6 justify-center`
 const activeLinkClass = ({ isActive }) =>
-  `text-2xl font-bold no-underline hover:text-orange-500 ${
-    isActive ? 'text-orange-500' : 'text-orange-300'
+  `text-2xl font-bold no-underline hover:text-orange-400 ${
+    isActive ? 'text-orange-400' : 'text-orange-300'
   }`
 
 const MainNavigation = () => (
